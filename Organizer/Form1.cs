68ddaa99c9f6f378c7fee08a684f6aebcce9d8e4
Dell <Dell@DELL-CORE2-01>
2017-06-29 17:59:15 +0300

    try connect to db

--- Organizer/Form1.cs
+++ Organizer/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,63 @@ namespace Organizer
         {
             InitializeComponent();
         }
+
+        TestDB db = new TestDB();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*   // Create and save a new Blog 
+               Console.Write("Enter a name for a new Blog: ");
+               var name = Console.ReadLine();
+
+               var blog = new Blog { Name = name };
+               db.Blogs.Add(blog);
+               db.SaveChanges();
+
+               // Display all Blogs from the database 
+               var query = from b in db.Blogs
+                           orderby b.Name
+                           select b;
+
+               Console.WriteLine("All blogs in the database:");
+               foreach (var item in query)
+               {
+                   Console.WriteLine(item.Name);
+               }
+
+               Console.WriteLine("Press any key to exit...");
+               Console.ReadKey();*/
+              
+            
+            /* 
+                Customer customer = new Customer();
+
+                // Получить данные из формы с помощью средств
+                // привязки моделей ASP.NET
+                IValueProvider provider =
+                    new FormValueProvider(ModelBindingExecutionContext);
+                if (TryUpdateModel<Customer>(customer, provider))
+                {
+                    // Загрузить фото профиля с помощью средств .NET
+                    HttpPostedFile photo = Request.Files["photo"];
+                    if (photo != null)
+                    {
+                        BinaryReader b = new BinaryReader(photo.InputStream);
+                        customer.Photo = b.ReadBytes((int)photo.InputStream.Length);
+                    }
+
+                    // В этой точке непосредственно начинается работа с Entity Framework
+
+                    // Создать объект контекста
+                    SampleContext context = new SampleContext();
+
+                    // Вставить данные в таблицу Customers с помощью LINQ
+                    context.Customers.Add(customer);
+
+                    // Сохранить изменения в БД
+                    context.SaveChanges();*/
+            }
+        }
     }
+    
 }
---
+++ Organizer/Product.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizer
+{
+    public class Product
+    {
+        /// <summary>
+        /// Название программы.
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// Идентификация продукта, такая как серийный номер программного обеспечения, или номер штампа на аппаратном чипе.
+        /// </summary>
+        public string IdentifyingNumber;
+        /// <summary>
+        /// Дата, когда этот продукт был установлен в системе.
+        /// </summary>
+        public DateTime InstallDate;
+        /// <summary>
+        /// Расположение установленного продукта.
+        /// </summary>
+        public string InstallLocation;
+        /// <summary>
+        /// Название поставщика продукта. 
+        /// </summary>
+        public string Vendor;
+        /// <summary>
+        /// Версия
+        /// </summary>
+        public string Version;
+
+        public string Fields
+        {
+            get
+            {
+                return "Name;IdentifyingNumber;InstallDate;InstallLocation;Vendor;Version";
+            }
+        }
+
+        public string GetSign()
+        {
+            return "Product";
+        }
+
+        public List<Product> GetData()
+        {
+            List<Product> list = new List<Product>();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT " + "Caption,IdentifyingNumber,InstallDate,InstallLocation,Vendor,Version,InstallDate2" + " FROM Win32_Product");
+
+            foreach (ManagementObject item in searcher.Get())
+            {
+                list.Add(new Product()
+                {
+                    Name = (string)item["Caption"],
+                    IdentifyingNumber = (string)item["IdentifyingNumber"],
+                    InstallDate = ((DateTime)item["InstallDate"]),
+                    InstallLocation = (string)item["InstallLocation"],
+                    Vendor = (string)item["Vendor"],
+                    Version = (string)item["Version"]
+                });
+            }
+            return list;
+        }
+
+        public object this[string name]
+        {
+            get
+            {
+                switch (name)
+                {
+                    case "Name": return Name;
+                    case "IdentifyingNumber": return IdentifyingNumber;
+                    case "InstallDate": return InstallDate;
+                    case "InstallLocation": return InstallLocation;
+                    case "Vendor": return Vendor;
+                    case "Version": return Version;
+                    default:
+                        return null;
+                }
+            }
+            protected set
+            {
+                switch (name)
+                {
+                    case "Name": Name = (string)value; break;
+                    case "IdentifyingNumber": IdentifyingNumber = (string)value; break;
+                    case "InstallDate": InstallDate = (DateTime)value; break;
+                    case "InstallLocation": InstallLocation = (string)value; break;
+                    case "Vendor": Vendor = (string)value; break;
+                    case "Version": Version = (string)value; break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
